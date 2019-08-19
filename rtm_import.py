b646947b329cd6c97f4d00222f039e934f2ff99b
4d4a5852 <4d4a5852@users.noreply.github.com>
2019-08-19 21:16:33 +0200

    update year in header

--- rtm_import.py
+++ rtm_import.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 4d4a5852
+# Copyright (C) 2016-2019 4d4a5852
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
